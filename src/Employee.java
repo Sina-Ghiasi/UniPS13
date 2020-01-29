3bce6185f9eb5b7b21e046bb96e7041b2e8a2107
Sina Ghiasi <sinasteyn@gmail.com>
2020-01-29 18:54:05 +0330

    Signed-off-by: Sina Ghiasi <sinasteyn@gmail.com>

---
+++ src/Employee.java
@@ -0,0 +1,20 @@
+public class Employee {
+    private String name;
+    private String nationalId;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNationalId() {
+        return nationalId;
+    }
+
+    public void setNationalId(String nationalId) {
+        this.nationalId = nationalId;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,37 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class Main {
+    @FunctionalInterface
+    public interface Writer {
+       void write(Employee employee);
+    }
+    public static void main(String[] args) {
+        ArrayList<Employee> employeeArrayList = new ArrayList<>();
+        for(int i = 0;i<4;i++){
+            Employee employee =new Employee();
+            employee.setName("employee "+i);
+            employee.setNationalId("NID"+i);
+            employeeArrayList.add(employee);
+        }
+        Writer writer =(employee)->{
+        try{
+            Connection connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "sina","sg159753");
+            PreparedStatement preparedStatement = connection.prepareStatement("insert into employee (name,nationalID) values (?,?,?,?)");
+            preparedStatement.setString(1, employee.getName());
+            preparedStatement.setString(2, employee.getNationalId());
+            preparedStatement.executeUpdate();
+            preparedStatement.close();
+            connection.close();
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        };
+        for(Employee employee:employeeArrayList){
+            writer.write(employee);
+        }
+    }
+}
